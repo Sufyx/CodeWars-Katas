33a777d419f9eccf22c95b4bc8f2df4292d85264
Asaf G <asafg86@gmail.com>
2023-09-28 22:59:11 +0300

    Directory rearrangement

---
+++ GOlang/8 kyu/SumOfPositives.go
@@ -0,0 +1,15 @@
+/**
+ * https://www.codewars.com/kata/5715eaedb436cf5606000381
+ */
+
+package kata
+
+func PositiveSum(numbers []int) int {
+	sum := 0
+	for _, n := range numbers {
+		if n > 0 {
+			sum += n
+		}
+	}
+	return sum
+}
