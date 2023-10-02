ea487cc07d1830c53ac2b66293a4d63d76b7d98c
Asaf G <asafg86@gmail.com>
2023-10-02 11:40:37 +0300

    some more katas

---
+++ GOlang/6 kyu/Multiples_of_3_or_5.go
@@ -0,0 +1,15 @@
+/**
+ * https://www.codewars.com/kata/514b92a657cdc65150000006
+ */
+
+package kata
+
+func Multiple3And5(number int) int {
+	sum := 0
+	for i := 0; i < number; i++ {
+		if (i%3 == 0) || (i%5 == 0) {
+			sum += i
+		}
+	}
+	return sum
+}
